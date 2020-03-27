dbd7a4fab4798e74ab03bb4f718c73eefe6ebd4c
matbanasik <mateuszbanasik22@gmail.com>
2020-03-28 00:30:17 +0100

    setup testing environment

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+test ('App renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+})
