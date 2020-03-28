0f73ae073e1f2ab14c1440a78223f593a729ab44
matbanasik <mateuszbanasik22@gmail.com>
2020-03-28 13:39:38 +0100

    complete eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": ["airbnb", "prettier"],
+    "extends": ["airbnb", "prettier", "plugin:react/recommended"],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -15,9 +15,28 @@ module.exports = {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
-    "plugins": ["react", "prettier"],
+    "plugins": ["react", "react-hooks", "prettier"],
     "rules": {
         "react/jsx-indent": [1, "tab"],
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/jsx-uses-react": "error",   
+        "react/jsx-uses-vars": "error",
+        "indent": [
+            'error',
+            4,
+            {
+                SwitchCase: 1
+            }
+        ],
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "react/jsx-filename-extension": [
+            "error",
+            {
+                extensions: [".js"]
+            }
+        ],
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn"
     }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -14,15 +14,16 @@ module.exports = {
     },
     module: {
         rules: [
-          {
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"]
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/transform-runtime"]
+                },
             }
-          }
         ]
-      },
+    },
     plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })]
 };
